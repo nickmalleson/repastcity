fbe6d5ac23f77c33b3b9d49e0140ba5143f0e78b
nick.malleson <nick.malleson@1733008d-a585-9dc6-b200-bda1ff30331b>
2012-01-06 08:52:28 +0000

    Initial commit.

---
+++ repastcity3/src/repastcity3/exceptions/AgentCreationException.java
@@ -0,0 +1,33 @@
+/*
+©Copyright 2012 Nick Malleson
+This file is part of RepastCity.
+
+RepastCity is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RepastCity is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RepastCity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package repastcity3.exceptions;
+
+public class AgentCreationException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public AgentCreationException(String message) {
+		super(message);
+	}
+	
+	public AgentCreationException(Throwable cause) {
+		super(cause);
+	}
+
+}
